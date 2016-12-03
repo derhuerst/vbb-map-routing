9f647a70e22c5d8c91e6dee3868b6ccbaaa53cbe
Jannis R <mail@jannisr.de>
2016-12-03 17:25:01 +0100

    embed map

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 'use strict'
 
-// todo
+const document = require('global/document')
+
+const src = require('./map.svg')
+
+const wrapper = document.createElement('div')
+wrapper.innerHTML = src
+const map = wrapper.querySelector('svg')
+document.body.appendChild(map)
+map.classList.add('map')
+map.setAttribute('preserveAspectRatio', 'xMidYMid meet')
