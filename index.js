2c0bab9488321ed7f8ba3f9e2b9a35c6d87726cc
Jannis R <mail@jannisr.de>
2016-12-04 03:40:28 +0100

    slice lines

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 const document = require('global/document')
 const vbb = require('vbb-client')
 const debounce = require('debounce')
+const simplify = require('simplify-geometry')
+const absPath = require('abs-svg-path')
+const parsePath = require('parse-svg-path')
+const lineSlice = require('@turf/line-slice')
 
 const src = require('./map.svg')
 
@@ -12,6 +16,73 @@ const map = wrapper.querySelector('svg')
 document.body.appendChild(map)
 map.classList.add('map')
 map.setAttribute('preserveAspectRatio', 'xMidYMid meet')
+
+
+
+const findClosestDistanceOnPath = (px, py, path) => {
+	const fitness = (d) => {
+		const {x, y} = path.getPointAtLength(d)
+		return Math.sqrt(Math.pow(x - px, 2) + Math.pow(y - py, 2))
+	}
+	const range = path.getTotalLength()
+
+	let lowerBoundary = 0
+	let upperBoundary = path.getTotalLength()
+	let bestD = null
+	let bestF = 10000000
+	for (let resolution = range / 2; resolution > (range / 100); resolution /= 2) {
+
+		bestD = null
+		bestF = 10000000
+		for (let d = lowerBoundary + resolution; d < upperBoundary; d += resolution) {
+			const f = fitness(d)
+			if (f < bestF) {
+				bestD = d
+				bestF = f
+			}
+		}
+
+		lowerBoundary = Math.max(bestD - resolution, 0)
+		upperBoundary = Math.min(bestD + resolution, range)
+	}
+
+	return bestD
+}
+
+const linearizePath = (path, resolution) => {
+	const points = []
+	const length = path.getTotalLength()
+
+	for (let d = 0; d < length; d += resolution) {
+		const {x, y} = path.getPointAtLength(d)
+		points.push([x, y])
+	}
+
+	return points
+	// return simplify(points, resolution)
+}
+
+const slicePath = (path, from, to) => {
+	const points = linearizePath(path, .5)
+
+	from = path.getPointAtLength(from)
+	from = {type: 'Feature', 'properties': {}, 'geometry': {
+		type: 'Point', coordinates: [from.x, from.y]
+	}}
+	to = path.getPointAtLength(to)
+	to = {type: 'Feature', 'properties': {}, 'geometry': {
+		type: 'Point', coordinates: [to.x, to.y]
+	}}
+
+	const sliced = lineSlice(from, to, {
+		type: 'Feature', properties: {},
+		geometry: {type: 'LineString', coordinates: points}
+	}).geometry.coordinates
+	.map((p) => (Math.round(p[0] * 100) / 100) + ' ' + (Math.round(p[1] * 100) / 100))
+	.join(' L ')
+
+	return 'M ' + sliced
+}
 
 
 
@@ -37,12 +108,29 @@ const render = (route) => {
 			if (el) el.classList.remove('inactive')
 		}
 
-		const line = (part.product || {}).line
-		if (line) {
-			const lineEl = document.querySelector('#line-' + line)
-			if (lineEl) lineEl.classList.remove('inactive')
-			else console.error('could not find line', line)
-		} else console.error('did not get line')
+		const from = document.querySelector('#station-' + part.from.id)
+		const to = document.querySelector('#station-' + part.to.id)
+
+		const lineEl = document.querySelector('#line-' + part.product.line)
+		if (from && to && lineEl) {
+
+			const fromBBox = from.getBBox()
+			const fromX = fromBBox.x + fromBBox.width / 2
+			const fromY = fromBBox.y + fromBBox.height / 2
+			const fromPos = findClosestDistanceOnPath(fromX, fromY, lineEl)
+
+			const toBBox = to.getBBox()
+			const toX = toBBox.x + toBBox.width / 2
+			const toY = toBBox.y + toBBox.height / 2
+			const toPos = findClosestDistanceOnPath(toX, toY, lineEl)
+
+			const segment = document.createElementNS('http://www.w3.org/2000/svg', 'path')
+			segment.setAttribute('d', slicePath(lineEl, fromPos, toPos))
+			segment.style.stroke = window.getComputedStyle(lineEl).stroke
+			segment.style.strokeWidth = '3'
+			lineEl.parentNode.appendChild(segment)
+
+		} else console.error('invalid data')
 	}
 }
 
