c24dffb86ef3d2a83aed12bf5728fa4c5d6dc283
Jannis R <mail@jannisr.de>
2017-02-13 01:02:13 +0100

    render all parts of a route

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const diff = require('virtual-dom/diff')
 const patch = require('virtual-dom/patch')
 // const ms = require('ms')
 
+const data = require('bvg-topological-map/index.json')
 const renderMap = require('./lib/render-map')
 const closestDistanceOnPath = require('./lib/closest-distance-on-path')
 const slicePath = require('./lib/slice-path')
@@ -18,8 +19,7 @@ const slicePath = require('./lib/slice-path')
 const state = {
 	from: null,
 	to: null,
-	line: null,
-	passed: [],
+	stations: [],
 	slices: []
 }
 
@@ -30,41 +30,44 @@ const addStation = (id) => {
 	} else if (state.from) state.to = id
 	else state.from = id
 
-	state.passed = []
+	state.stations = []
 	state.slices = []
-	state.line = null
+
 	if (state.from && state.to) fetch()
 	rerender()
 }
 
 const setRoute = (route) => {
-	const part = route.parts[0] // todo: support all parts
-	const from = part.from.id
-	const to = part.to.id
-	const line = part.product.line
+	state.stations = []
+	state.slices = []
 
-	state.from = from
-	state.to = to
-	state.line = line
-	state.passed = part.passed.map((passed) => passed.station.id)
+	for (let part of route.parts) {
+		const from = part.from.id
+		const to = part.to.id
+		const line = part.product ? part.product.line : null
 
-	const fromEl = document.querySelector('#station-' + from)
-	const toEl = document.querySelector('#station-' + to)
-	const lineEl = document.querySelector('#line-' + line)
+		const fromEl = document.querySelector('#station-' + from)
+		const toEl = document.querySelector('#station-' + to)
+		const lineEl = document.querySelector('#line-' + line)
 
-	if (fromEl && toEl && lineEl) {
-		const fromBBox = fromEl.getBBox()
-		const fromX = fromBBox.x + fromBBox.width / 2
-		const fromY = fromBBox.y + fromBBox.height / 2
-		const fromPos = closestDistanceOnPath(fromX, fromY, lineEl)
+		if (fromEl && toEl && lineEl) {
+			state.stations.push(from)
+			part.passed.forEach((passed) => state.stations.push(passed.station.id))
+			state.stations.push(from)
 
-		const toBBox = toEl.getBBox()
-		const toX = toBBox.x + toBBox.width / 2
-		const toY = toBBox.y + toBBox.height / 2
-		const toPos = closestDistanceOnPath(toX, toY, lineEl)
+			const fromBBox = fromEl.getBBox()
+			const fromX = fromBBox.x + fromBBox.width / 2
+			const fromY = fromBBox.y + fromBBox.height / 2
+			const fromPos = closestDistanceOnPath(fromX, fromY, lineEl)
 
-		const path = slicePath(lineEl, fromPos, toPos)
-		state.slices = [{path, line}]
+			const toBBox = toEl.getBBox()
+			const toX = toBBox.x + toBBox.width / 2
+			const toY = toBBox.y + toBBox.height / 2
+			const toPos = closestDistanceOnPath(toX, toY, lineEl)
+
+			const shape = slicePath(lineEl, fromPos, toPos)
+			state.slices.push({shape, line})
+		}
 	}
 
 	rerender()
