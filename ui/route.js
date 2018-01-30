6af72c7a13795f19fc18899669684dfb966976c9
Jannis R <mail@jannisr.de>
2018-01-30 22:54:20 +0100

    adapt to FPTF 1.0.1

--- ui/route.js
+++ ui/route.js
@@ -5,7 +5,7 @@ const journey = require('vbb-journey-ui')
 const renderJourney = (state, actions) => {
 	const a = Object.create(actions)
 	a.selectStation = actions.setHighlight
-	return journey(state.route, state.details, a)
+	return journey(state.journey, state.details, a)
 }
 
 module.exports = renderJourney
